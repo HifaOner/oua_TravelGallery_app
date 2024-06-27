1c3002bb39dea2910bcba2f1d63392d80abe42b1
HifaOner <hifaoner@gmail.com>
2024-06-27 20:12:47 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/travel_gallery_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.travel_gallery_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:travel_gallery_app/admin/admin_login.dart';
+import 'package:travel_gallery_app/views/bottom_nav.dart';
+import 'package:travel_gallery_app/views/home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(
+              seedColor: Color.fromARGB(255, 170, 165, 179)),
+          useMaterial3: true,
+        ),
+        home: const Home());
+  }
+}
