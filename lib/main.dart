880826302a7ffbfa52c48f09edf28a19bdbc91c7
HifaOner <hifaoner@gmail.com>
2024-07-05 16:34:30 +0300

    firebase_installation

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:travel_gallery_app/admin/admin_login.dart';
 import 'package:travel_gallery_app/views/bottom_nav.dart';
 import 'package:travel_gallery_app/views/home.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
